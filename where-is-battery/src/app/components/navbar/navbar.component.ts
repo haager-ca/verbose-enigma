c9f9c185eb689c1ed5b0c91d13c9ce8963bbddbc
haager-ca <caius.haager@icloud.com>
2021-06-20 19:50:42 +0200

    added toggler button

--- where-is-battery/src/app/components/navbar/navbar.component.ts
+++ where-is-battery/src/app/components/navbar/navbar.component.ts
@@ -6,12 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  // Indicates if the navbar is collapsed -> not visible
+  public navbarCollapsed = true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  public toggleNavbar(): void {
+    this.navbarCollapsed = !this.navbarCollapsed;
+  }
 }
 
 console.log("Here is an Error Message Navbar")
